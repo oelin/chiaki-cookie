4b053147a7bf7eb77da10038849b60d622620ee5
user <user@example.com>
2023-03-18 13:53:19 +0000

    fix package.json

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 export default response =>
         Object.fromEntries(response
                 .headers['set-cookie']
-                .map(cookie => [
-                        cookie.split('=')[0],                            // Name.
-                        cookie.split('=').slice(1).join('').split('; '), // Value.
-                ])
+                .map(cookie => cookie.split(';')[0])
+		.map(cookie => cookie.split('='))
         )
